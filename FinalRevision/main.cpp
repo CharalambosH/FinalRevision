6622647cd4b7eeeb55b521a32478e598382b8b0e
Charalambos Hadjigeorgiou <hadjigeorgiou.c3@live.unic.ac.cy>
2024-05-26 14:31:21 +0300

    Initial Commit

---
+++ FinalRevision/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  FinalRevision
+//
+//  Created by Charalambos Hadjigeorgiou on 26/05/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
