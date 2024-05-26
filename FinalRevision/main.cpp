cf6217d12d9ae66554e3e953582ed739961794d7
Charalambos Hadjigeorgiou <hadjigeorgiou.c3@live.unic.ac.cy>
2024-05-26 15:27:58 +0300

    Initial commit

--- FinalRevision/main.cpp
+++ FinalRevision/main.cpp
@@ -1,14 +1,48 @@
-//
-//  main.cpp
-//  FinalRevision
-//
-//  Created by Charalambos Hadjigeorgiou on 26/05/2024.
-//
+/** \file FinalRevision
+ * \brief Small Math Program
+ * \author Charalambos Hadjigeorgiou
+ * \date 26/05/2024
+ */
 
-#include <iostream>
+#include<iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+//prototypes
+double sumOfRange(double arr[], int startIndex, int endIndex, int maxSize);
+double productOfRange(double arr[], int startIndex, int endIndex, int maxSize);
+bool allPositive(double arr[], int maxSize);
+
+int main() {
+    
+    const int maxSize = 5;
+    double arr [maxSize] = {1.3, 2.2, -3.5, 0.0, 10.2};
+    int choice;
+    
+    do {
+        
+        cout << "\n1) Sum of range";
+        cout << "\n2) Product of range";
+        cout << "\n3) Check if all numbers are positive";
+        cout << "\n4) EXIT!";
+        
+        switch (choice) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            default:
+                cerr << "Wrong input! Please try again :)";
+                break;
+        }
+                while (choice != 4)
+                    return 0;
+        }
+    
+    }
+
 }
